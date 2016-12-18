5beeb2a4ea91eb25d4e66c932e440d536691ce03
mazdev <majid_2007j@yahoo.co.in>
2016-12-19 00:02:55 +0530

    adding scripts to send params other than pwd for authentication

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,6 +13,13 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def user_id_logged_in?(id)
+    logger.info session[:user_id]
+    logger.info id
+    session[:user_id]==id
+      
+  end
+
  # Logs out the current user.
   def log_out
     session.delete(:user_id)
